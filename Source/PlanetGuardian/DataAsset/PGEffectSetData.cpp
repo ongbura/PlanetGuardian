4a7a65e02feffaf85774d8a0d1dc0fac940c905c
ongbular <ongbular@gmail.com>
2023-05-02 02:14:46 +0900

    feat: Add Guardian Foot Step Effect

--- Source/PlanetGuardian/DataAsset/PGEffectSetData.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) 2023 Ongbular(Park Joo Hyeong). All rights reserved.
-
-
-#include "PGEffectSetData.h"
-
-FPrimaryAssetId UPGEffectSetData::GetPrimaryAssetId() const
-{
-	return { GetPrimaryAssetType(), GetFName() };
-}
